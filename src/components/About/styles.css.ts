69b0deb2e63051c17a38b4b781dc72d937e5ec56
Ollie Beckwith <oliver.beckwith1@gmail.com>
2022-04-13 18:14:59 +0100

    Add scaling to About styles

--- src/components/About/styles.css.ts
+++ src/components/About/styles.css.ts
@@ -1,14 +1,52 @@
 import styled from "styled-components"
+import { device } from "../../utils/media";
 
 export const AboutContainer = styled.div`
+  padding: 0 8px;
+
+  ${device.tablet} {
+    padding: 0 16px;
+  }
+  ${device.laptop} {
+    padding: 16px 32px;
+  }
+  ${device.desktop} {
+    padding: 16px 64px;
+  }
 `;
 
 export const AboutHeader = styled.h2`
   font-weight: 500;
+  text-align: center;
+  font-size: 24px;
+
+  ${device.tablet} {
+    text-align: left;
+  }
+  ${device.laptop} {
+    font-size: 36px;
+    font-weight: 400;
+  }
 `;
 
 export const AboutDescription = styled.p`
-  line-height: 24px;
+  line-height: 22px;
+  text-align: center;
+  font-size: 14px;
+
+  ${device.mobile} {
+    font-size: 16px;
+    line-height: 24px;
+  }
+  ${device.tablet} {
+    font-size: 18px;
+    line-height: 26px;
+    text-align: left;
+  }
+  ${device.desktop} {
+    font-size: 20px;
+    line-height: 28px;
+  }
 `;
 
 export const Strong = styled.strong`
@@ -24,5 +62,11 @@ export const Strong = styled.strong`
 
   &:hover {
     border-bottom-width: 2px;
+    ${device.tablet} {
+      border-bottom-width: 3px;
+    }
+    ${device.desktop} {
+      border-bottom-width: 4px;
+    }
   }
 `;
