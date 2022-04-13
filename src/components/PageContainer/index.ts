c749b13c67234d69f777e2903baed80775300061
Ollie Beckwith <oliver.beckwith1@gmail.com>
2022-04-13 18:14:27 +0100

    Add shadow to content container above 1440px

--- src/components/PageContainer/index.ts
+++ src/components/PageContainer/index.ts
@@ -1,12 +1,21 @@
 import styled from "styled-components";
+import { device } from "../../utils/media";
 
 export const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  ${device.widescreen} {
+    background-color: #fbfbfb;
+  }
 `;
 
 export const ContentContainer = styled.div`
   max-width: 1440px;
   width: 100%;
+  background-color: white;
+  ${device.widescreen} {
+    min-height: 100vh;
+    box-shadow: 0 0 16px grey;
+  }
 `;
